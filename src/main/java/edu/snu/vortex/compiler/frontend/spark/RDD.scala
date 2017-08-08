7c4d9f19bff28fc81c500481b74e3e2928e3d27e
Won Wook SONG <wsong0512@gmail.com>
2017-08-08 18:26:02 +0900

    workin

--- src/main/java/edu/snu/vortex/compiler/frontend/spark/RDD.scala
+++ src/main/java/edu/snu/vortex/compiler/frontend/spark/RDD.scala
@@ -15,9 +15,83 @@
  */
 package edu.snu.vortex.compiler.frontend.spark
 
+import scala.reflect.ClassTag
+
 /**
   * Created by wonook on 07/08/2017.
   */
-abstract class RDD {
+abstract class RDD[T: ClassTag](@transient private var _sc: SparkContext,
+                                @transient private var deps: Seq[Dependency[_]]) extends Serializable {
+  private def sc: SparkContext = {
+    _sc
+  }
+
+  def this(@transient oneParent: RDD[_]) =
+    this(oneParent.context, List(new OneToOneDependency(oneParent)))
+
+  private[spark] def conf = sc.conf
+
+  def sparkContext: SparkContext = sc
+
+  @transient var name: String = null
+
+  def setName(_name: String): this.type = {
+    name = _name
+    this
+  }
+
+  def map[U: ClassTag](f: T => U): RDD[U] = withScope {
+    val cleanF = sc.clean(f)
+    new MapPartitionsRDD[U, T](this, (context, pid, iter) => iter.map(cleanF))
+  }
+
+  def flatMap[U: ClassTag](f: T => TraversableOnce[U]): RDD[U] = withScope {
+    val cleanF = sc.clean(f)
+    new MapPartitionsRDD[U, T](this, (context, pid, iter) => iter.flatMap(cleanF))
+  }
+
+  def filter(f: T => Boolean): RDD[T] = withScope {
+    val cleanF = sc.clean(f)
+    new MapPartitionsRDD[T, T](
+      this,
+      (context, pid, iter) => iter.filter(cleanF),
+      preservesPartitioning = true)
+  }
+
+  def distinct(numPartitions: Int)(implicit ord: Ordering[T] = null): RDD[T] = withScope {
+    map(x => (x, null)).reduceByKey((x, y) => x, numPartitions).map(_._1)
+  }
+
+  def distinct(): RDD[T] = withScope {
+    distinct(partitions.length)
+  }
+
+  def reduce(f: (T, T) => T): T = withScope {
+    val cleanF = sc.clean(f)
+    val reducePartition: Iterator[T] => Option[T] = iter => {
+      if (iter.hasNext) {
+        Some(iter.reduceLeft(cleanF))
+      } else {
+        None
+      }
+    }
+    var jobResult: Option[T] = None
+    val mergeResult = (index: Int, taskResult: Option[T]) => {
+      if (taskResult.isDefined) {
+        jobResult = jobResult match {
+          case Some(value) => Some(f(value, taskResult.get))
+          case None => taskResult
+        }
+      }
+    }
+    sc.runJob(this, reducePartition, mergeResult)
+    // Get the final result out of our Option, or throw an exception if the RDD was empty
+    jobResult.getOrElse(throw new UnsupportedOperationException("empty collection"))
+  }
+
+
+}
+
+object RDD {
 
 }
--- src/main/java/edu/snu/vortex/compiler/frontend/spark/SparkContext.scala
+++ src/main/java/edu/snu/vortex/compiler/frontend/spark/SparkContext.scala
@@ -15,7 +15,7 @@
  */
 package edu.snu.vortex.compiler.frontend.spark
 
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
 
 import scala.collection.Map
 import scala.reflect.ClassTag
@@ -24,6 +24,9 @@ import scala.reflect.ClassTag
   * Created by wonook on 07/08/2017.
   */
 class SparkContext(config: SparkConf) {
+  private val allowMultipleContexts: Boolean =
+    config.getBoolean("spark.driver.allowMultipleContexts", false)
+
   private[spark] val stopped: AtomicBoolean = new AtomicBoolean(false)
 
   def this() = this(new SparkConf())
@@ -45,7 +48,15 @@ class SparkContext(config: SparkConf) {
   private[spark] def this(master: String, appName: String, sparkHome: String, jars: Seq[String]) =
     this(master, appName, sparkHome, jars, Map())
 
+  private var _conf: SparkConf = _
+  private var _applicationId: String = _
+  private var _applicationAttemptId: Option[String] = None
+  private var _jars: Seq[String] = _
+  private var _files: Seq[String] = _
+
   private[spark] def conf: SparkConf = _conf
+
+  def getConf: SparkConf = conf.clone()
 
   def jars: Seq[String] = _jars
   def files: Seq[String] = _files
@@ -54,6 +65,10 @@ class SparkContext(config: SparkConf) {
   def appName: String = _conf.get("spark.app.name")
 
   def isStopped: Boolean = stopped.get()
+
+  def applicationId: String = _applicationId
+  def applicationAttemptId: Option[String] = _applicationAttemptId
+
 
   def parallelize[T: ClassTag](seq: Seq[T],
                                numSlices: Int = defaultParallelism): RDD[T] = withScope {
@@ -65,6 +80,55 @@ class SparkContext(config: SparkConf) {
     // Still track the stopped variable for use elsewhere in the code.
     if (!stopped.compareAndSet(false, true)) {
       return
+    }
+  }
+
+
+
+  def runJob[T, U: ClassTag](rdd: RDD[T],
+                             processPartition: Iterator[T] => U,
+                             resultHandler: (Int, U) => Unit) {
+    val processFunc = (context: TaskContext, iter: Iterator[T]) => processPartition(iter)
+    runJob[T, U](rdd, processFunc, 0 until rdd.partitions.length, resultHandler)
+  }
+
+  private[spark] def clean[F <: AnyRef](f: F, checkSerializable: Boolean = true): F = {
+    // INCOMPLETE
+    if (f == null) {
+      return f
+    }
+    f
+  }
+
+  SparkContext.setActiveContext(this, allowMultipleContexts)
+}
+
+object SparkContext {
+  private val SPARK_CONTEXT_CONSTRUCTOR_LOCK = new Object()
+
+  private val activeContext: AtomicReference[SparkContext] =
+    new AtomicReference[SparkContext](null)
+
+  private var contextBeingConstructed: Option[SparkContext] = None
+
+  def getOrCreate(config: SparkConf): SparkContext = {
+    SPARK_CONTEXT_CONSTRUCTOR_LOCK.synchronized {
+      if (activeContext.get() == null) {
+        setActiveContext(new SparkContext(config), allowMultipleContexts = false)
+      }
+      activeContext.get()
+    }
+  }
+
+  def getOrCreate(): SparkContext = {
+    getOrCreate(new SparkConf())
+  }
+
+  private[spark] def setActiveContext(sc: SparkContext,
+                                      allowMultipleContexts: Boolean): Unit = {
+    SPARK_CONTEXT_CONSTRUCTOR_LOCK.synchronized {
+      contextBeingConstructed = None
+      activeContext.set(sc)
     }
   }
 }
