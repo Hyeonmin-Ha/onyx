547d916a1137b123519145b76b6b740c93d622d6
sanha <sanhaleehana@naver.com>
2017-10-25 14:11:21 +0900

    use ser mem store, 8 red

--- src/main/java/edu/snu/onyx/compiler/optimizer/pass/compiletime/annotating/SmallScaleMemoryPass.java
+++ src/main/java/edu/snu/onyx/compiler/optimizer/pass/compiletime/annotating/SmallScaleMemoryPass.java
@@ -20,7 +20,7 @@ import edu.snu.onyx.compiler.ir.IREdge;
 import edu.snu.onyx.compiler.ir.IRVertex;
 import edu.snu.onyx.compiler.ir.executionproperty.ExecutionProperty;
 import edu.snu.onyx.compiler.ir.executionproperty.edge.DataStoreProperty;
-import edu.snu.onyx.runtime.executor.data.MemoryStore;
+import edu.snu.onyx.runtime.executor.data.SerializingMemoryStore;
 import edu.snu.onyx.runtime.executor.datatransfer.communication.ScatterGather;
 
 import java.util.stream.Collectors;
@@ -43,7 +43,7 @@ public final class SmallScaleMemoryPass extends AnnotatingPass {
     dag.topologicalDo(irVertex ->
         dag.getIncomingEdgesOf(irVertex).forEach(irEdge -> {
           if (ScatterGather.class.equals(irEdge.getProperty(ExecutionProperty.Key.DataCommunicationPattern))) {
-            irEdge.setProperty(DataStoreProperty.of(MemoryStore.class));
+            irEdge.setProperty(DataStoreProperty.of(SerializingMemoryStore.class));
           }
         }));
     return dag;
