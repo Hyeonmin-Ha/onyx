1921ef6c60ad0b3fa9b73a913e047b7e20f0cb49
Jeongyoon Eo <jeongyoon0807@gmail.com>
2017-11-09 11:39:33 +0900

    revert executor capacity - no need to have multiple threads

--- src/main/java/edu/snu/onyx/client/JobConf.java
+++ src/main/java/edu/snu/onyx/client/JobConf.java
@@ -113,10 +113,8 @@ public final class JobConf extends ConfigurationModuleBuilder {
    * 1) Master's TaskGroup scheduler can use this number in scheduling.
    *    (e.g., schedule TaskGroup to the executor currently with the maximum number of available slots)
    * 2) Executor's number of TaskGroup execution threads is set to this number.
-   *    For intra- and inter-TaskGroup element-wise pipelining, Tasks should run in parallel,
-   *    hence default value is set to larger than 1.
    */
-  @NamedParameter(doc = "Executor capacity", short_name = "executor_capacity", default_value = "5")
+  @NamedParameter(doc = "Executor capacity", short_name = "executor_capacity", default_value = "1")
   public final class ExecutorCapacity implements Name<Integer> {
   }
 
